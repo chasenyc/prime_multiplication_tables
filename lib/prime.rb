d748f4178a1aa33a374253eb732423cc636ea2bd
Alex Harris <chasenyc@gmail.com>
2016-02-15 12:28:37 -0500

    Add Prime.first, which gathers n amount of prime numbers and returns an array

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,7 +1,20 @@
 class Prime
 
+  def self.first(amount)
+    primes = []
+
+    i = 2
+    until primes.size >= amount
+      primes << i if is_prime?(i)
+      i += 1
+    end
+
+    primes
+  end
+
   def self.is_prime?(num)
     return false if num < 2
     (2...num).none? { |factor| num % factor == 0 }
   end
+  
 end
