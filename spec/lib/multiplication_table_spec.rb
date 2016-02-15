300cec9971045795e07a3b176f85ee6fef96a2e3
Alex Harris <chasenyc@gmail.com>
2016-02-15 13:10:58 -0500

    Add tests for generate_table_edges.

--- spec/lib/multiplication_table_spec.rb
+++ spec/lib/multiplication_table_spec.rb
@@ -1,11 +1,18 @@
 require_relative '../../lib/multiplication_table'
 
 describe MultiplicationTable do
-  let(:table) { MultiplicationTable.new(4) }
+  let(:table_one) { MultiplicationTable.new(4) }
 
   it 'should generate a two dimensional array (N+1)x(N+1)' do
-    expect(table.table.length).to eq 5
-    expect(table.table.first.length).to eq 5
+    expect(table_one.table.length).to eq 5
+    expect(table_one.table.first.length).to eq 5
   end
-  
+
+  it 'generate_table_edges should populate top and left side with primes' do
+    table_one.generate_table_edges
+    expect(table_one.table[0]).to eq [" ", 2, 3, 5, 7]
+    expect(table_one.table[2][0]).to eq 3
+    expect(table_one.table[4][0]).to eq 7
+    expect(table_one.table[2][2]).to eq nil
+  end
 end
